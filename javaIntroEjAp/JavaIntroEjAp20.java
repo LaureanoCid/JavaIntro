5b9d2e9aaa712cc4552626ffa0e21f5384e57907
LaureanoCid <laurycid@gmail.com>
2023-03-13 21:06:58 -0300

    Ejercicios al dia

--- javaIntroEjAp/JavaIntroEjAp20.java
+++ javaIntroEjAp/JavaIntroEjAp20.java
@@ -7,8 +7,29 @@ están entre el 1 y el 9.
  */
 package javaIntroEjAp;
 
+import java.util.Scanner;
+
 public class JavaIntroEjAp20 {
 
-    public static void main(String[] args) {    
+    public static void main(String[] args) { 
+        
+        Scanner leer = new Scanner(System.in);
+        
+        int filas = 3, columnas = 3, num;
+        
+        int [] [] cuboMagico = new int [filas][columnas];
+        
+        //Llenado de Matriz con un numero entre 1-9
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                do {
+                    System.out.println("Ingrese un numero entre 1-9: ");
+                    num = leer.nextInt();
+                } while (num<1 || num>9);
+                
+                cuboMagico[i][j] += num;
+                                                       
+            }   
+        }
     }    
 }
